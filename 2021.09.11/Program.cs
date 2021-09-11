db63b3d72c7503828a6d13b3daa64c23c5fdd8f6
renzoregio <renzorregio@gmail.com>
2021-09-11 16:37:31 -0700

    assignment_one

---
+++ 2021.09.11/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _11._09._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                Console.Write("{0}\n", i);
+                
+            }
+        }
+    }
+}
